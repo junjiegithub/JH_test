a924a75374b6d3952c880d7d3e3d298eb783c4d1
Administrator <1416279053@qq.com>
2021-04-25 14:34:26 +0800

    随机生成字符串

--- venv/test_0420/zhi_guang_test.py
+++ venv/test_0420/zhi_guang_test.py
@@ -14,18 +14,24 @@ import string
 #     return l
 # fun()
 
-def fun():
-    s=""
-    for i in range(0,4):
-        if random.randint(0,1)==0:
-            #生成字母
-            s=s+chr(random.randint(65,90))
-        else:
-            #生成数字
-            s=s+str(random.randint(0,9))
-    print(s)
-    return s
 
-fun()
+#coding=utf-8
+#随机生成4位字符
 
-"""sq_data="""{"merchantId":1011,"accountList":[{"accountName":"""+c+""","timeZone":2,"adsName":"","creditCode":"","license":"","domains":"www.baidu.com","gmails":"665@qq.com"}],"num":1}""""""
+
+# #随机生成字符串
+# import random
+# import string
+# def fun():
+#     num=string.ascii_letters+string.digits
+#     print(num)
+#     l=[]
+#     for i in range(28):
+#         a=random.choice(num)
+#         l.append(a)
+#     print(''.join(l))
+#     return ''.join(l)
+# a=fun()
+#
+# print(fun())
+#"""sq_data="""{"merchantId":1011,"accountList":[{"accountName":"""+c+""","timeZone":2,"adsName":"","creditCode":"","license":"","domains":"www.baidu.com","gmails":"665@qq.com"}],"num":1}""""""
